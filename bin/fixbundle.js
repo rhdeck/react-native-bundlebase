c958ad00757ae9d691919ee3af73d01217ff7db0
Ray Deck <ray@raydeck.com>
2018-10-27 11:34:50 -0400

    Adding support for updating the project.pbxproj, which is how RN handles the default now.

--- bin/fixbundle.js
+++ bin/fixbundle.js
@@ -3,67 +3,40 @@ const glob = require("glob");
 const fs = require("fs");
 const path = require("path");
 const plist = require("plist");
-const deasync = require("deasync-promise");
 const inquirer = require("inquirer");
-//Get my directory
-var thisPath = process.argv[1];
-var thisPath = path.dirname(thisPath); //bin directory
-var thisPath = path.dirname(thisPath); //dependency directory
-var privacyText = "This app requires the camera to function";
-const myPackagePath = thisPath + "/package.json";
-if (fs.existsSync(myPackagePath)) {
-  const mypackage = require(myPackagePath);
-  if (typeof mypackage.IOSCameraPrivacyText != "undefined") {
-    privacyText = mypackage.IOSCameraPrivacyText;
-  }
-}
+const xcode = require("xcode");
+(async () => {
+  //Get my directory
+  var thisPath = process.cwd();
+  var plists = glob.sync(path.join(thisPath, "ios", "*", "Info.plist"));
+  var pbxprojs = glob.sync(path.join(thisPath, "ios", "*", "project.pbxproj"));
+  const defaultBase = "org.reactjs.native.example";
+  let found =
+    plists.find(path => {
+      const source = fs.readFileSync(path, "utf8");
+      const o = plist.parse(source);
+      return (
+        o.CFBundleIdentifier && o.CFBundleIdentifier.startsWith(defaultBase)
+      );
+    }) ||
+    pbxprojs.find(path => {
+      const source = fs.readFileSync(path, "utf8");
+      return source.indexOf(defaultBase) > -1;
+    });
 
-var thisPath = path.dirname(thisPath); // node_modules
-var baseName = path.basename(thisPath);
-if (!baseName.startsWith("node_modules")) {
-  console.log("This is not a dependency: ", thisPath);
-  process.exit();
-}
-var thisPath = path.dirname(thisPath); // parent
-const packagePath = thisPath + "/package.json";
-if (fs.existsSync(packagePath)) {
-  const package = require(packagePath);
-  if (typeof package.IOSCameraPrivacyText != "undefined") {
-    console.log("Setting from parent package");
-    privacyText = package.IOSCameraPrivacyText;
+  if (!found) return;
+
+  //FIgure out the new default
+  const homefile = process.env.HOME + "/.rninfo";
+  var homeinfo;
+  if (fs.existsSync(homefile)) {
+    homeinfo = JSON.parse(fs.readFileSync(homefile));
+  } else {
+    homeinfo = {};
   }
-}
-var iosPath = thisPath + "/ios";
-if (!fs.existsSync(iosPath)) {
-  console.log("Could not find ios in ", thisPath, iosPath);
-  console.log(fs.readdirSync(thisPath));
-  process.exit();
-}
-plists = glob.sync(iosPath + "/*/Info.plist");
-const homefile = process.env.HOME + "/.rninfo";
-var homeinfo;
-if (fs.existsSync(homefile)) {
-  homeinfo = JSON.parse(fs.readFileSync(homefile));
-} else {
-  homeinfo = {};
-}
-var needToAsk = false;
-var foundDefault = false;
-const defaultBase = "org.reactjs.native.example";
-plists.map(path => {
-  const source = fs.readFileSync(path, "utf8");
-  var o = plist.parse(source);
-  if (o.CFBundleIdentifier) {
-    if (o.CFBundleIdentifier.startsWith(defaultBase)) {
-      foundDefault = true;
-    }
-  }
-});
-if (foundDefault) {
   if (!homeinfo.bundleBase) {
-    console.log("HI there is no problem yet");
-    inquirer
-      .prompt([
+    try {
+      const answers = await inquirer.prompt([
         {
           message:
             "You do not have a saved bundlebase.\nWhat is the base ID you would use for your organization? (e.g. com.mycompany for an app that would be eventually com.mycompany.myapp)",
@@ -74,30 +47,18 @@ if (foundDefault) {
             return true;
           }
         }
-      ])
-      .then(
-        answers => {
-          const newbundle = answers.bundle;
-          homeinfo.bundleBase = newbundle;
-          fs.writeFileSync(homefile, JSON.stringify(homeinfo));
-          addToPlists();
-        },
-        err => {
-          console.log("Got an error", err);
-          process.exit(1);
-        }
-      );
-  } else {
-    addToPlists();
+      ]);
+      const newbundle = answers.bundle;
+      homeinfo.bundleBase = newbundle;
+      fs.writeFileSync(homefile, JSON.stringify(homeinfo));
+    } catch (e) {
+      console.log("Got an error", err);
+      process.exit(1);
+    }
   }
-}
-
-function addToPlists() {
-  plists.map(path => {
+  plists.forEach(path => {
     const source = fs.readFileSync(path, "utf8");
-    //console.log("Output:", source);
     var o = plist.parse(source);
-    //console.log("Parsed output", o);
     if (o.CFBundleIdentifier) {
       if (o.CFBundleIdentifier.startsWith(defaultBase)) {
         if (homeinfo.bundleBase) {
@@ -111,4 +72,23 @@ function addToPlists() {
       }
     }
   });
-}
+  pbxprojs.forEach(path => {
+    const project = xcode.project(path);
+    project.parseSync();
+    Object.keys(project.pbxXCBuildConfigurationSection())
+      .filter(k => !k.endsWith("_comment"))
+      .forEach(k => {
+        const o = project.pbxXCBuildConfigurationSection()[k];
+        if (!o.isa == "XCBuildConfiguration") return;
+        const oldName = o.buildSettings.PRODUCT_BUNDLE_IDENTIFIER;
+        if (!oldName) return;
+        if (oldName.startsWith('"' + defaultBase)) {
+          project.addBuildProperty(
+            "PRODUCT_BUNDLE_IDENTIFIER",
+            '"' + homeinfo.bundleBase + oldName.substring(defaultBase.length)
+          );
+        }
+      });
+    fs.writeFileSync(path, project.writeSync());
+  });
+})();
